7cc0b25097b975f4c24fc98380fb69087673c962
conradfr <yoyolebatteur@gmail.com>
2021-10-02 12:18:54 +0200

    Design wip

--- config/dev.exs
+++ config/dev.exs
@@ -1,13 +1,13 @@
 import Config
 
 # Configure your database
-config :funkyabx, FunkyABX.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "funkyabx_dev",
-  hostname: "localhost",
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+#config :funkyabx, FunkyABX.Repo,
+#  username: "postgres",
+#  password: "postgres",
+#  database: "funkyabx_dev",
+#  hostname: "localhost",
+#  show_sensitive_data_on_connection_error: true,
+#  pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
