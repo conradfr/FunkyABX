dc6d63409def18b978949b4bf8acd7e8f1807d38
conradfr <yoyolebatteur@gmail.com>
2021-12-01 08:42:33 +0100

    Add counter on homepage

--- lib/funkyabx_web/views/page_view.ex
+++ lib/funkyabx_web/views/page_view.ex
@@ -1,3 +1,15 @@
 defmodule FunkyABXWeb.PageView do
   use FunkyABXWeb, :view
+  import Ecto.Query, only: [from: 2]
+  alias FunkyABX.Repo
+  alias FunkyABX.Test
+
+  defp get_tests_total() do
+    query =
+      from(t in Test,
+        select: count(t.id)
+      )
+
+    Repo.one(query)
+  end
 end
