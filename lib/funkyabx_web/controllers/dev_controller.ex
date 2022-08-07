f5b54b355255e3c55867cbfb4c71336801a836d7
conradfr <yoyolebatteur@gmail.com>
2022-08-07 19:24:19 +0200

    [css] Put default color back for links (following theme update)

--- lib/funkyabx_web/controllers/dev_controller.ex
+++ lib/funkyabx_web/controllers/dev_controller.ex
@@ -2,6 +2,7 @@ defmodule FunkyABXWeb.DevController do
   use FunkyABXWeb, :controller
 
   # TODO cors for dev on cloud
+  # Edit: done but not used yet
 
   def redirect_file(conn, %{"part1" => part1, "part2" => part2} = _params) do
     Plug.Conn.send_file(conn, 200, "priv/static/uploads/#{part1}/#{part2}")
