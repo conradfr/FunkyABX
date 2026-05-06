eb55d45acb4fb42b5943c0b399ba7bed1dc77ce1
lcocatrix <lionel.cocatrix@gmail.com>
2026-05-06 21:35:14 +0200

    Refactor file download for visitors to prevent each one cancelling the previous one.

--- assets/js/hooks/DownloadTestFilesHook.js
+++ assets/js/hooks/DownloadTestFilesHook.js
@@ -1,25 +1,41 @@
-import cookies from '../utils/cookies';
-import localStorageUtils from '../utils/localStorageUtils';
-import { COOKIE_TEST_TAKEN } from '../config/config';
-
 /* eslint-disable no-undef */
 const DownloadTestFilesHook = {
   mounted() {
     this.handleEvent('download_file', (params) => {
-      const { title, url } = params;
-      const ext = new URL(url).pathname.split('.').pop();
-
-      console.log(title);
-      console.log(url);
-      console.log(ext);
-
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = `${title}.${ext}`;
-      document.body.appendChild(a);
-      a.click();
-      a.remove();
+      this.processDownload(params);
     });
+  },
+
+  /*
+    1/ The iframe is used to allow to launch multiple download in parallel.
+    check: https://stackoverflow.com/questions/2339440/download-multiple-files-with-a-single-action/9425731#9425731
+    2/ The blob is because Firefox ignores the download attribute on cross-origin URLs so we download the file first
+   */
+  async processDownload(params) {
+    const { title, url } = params;
+    const ext = new URL(url).pathname.split('.').pop();
+
+    try {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      const blobUrl = URL.createObjectURL(blob);
+
+      let iframe = document.createElement('iframe');
+      iframe.style.visibility = 'collapse';
+      document.body.append(iframe);
+
+      iframe.contentDocument.write(
+        `<a href="${blobUrl}" download="${title}.${ext}">`
+      );
+      iframe.contentDocument.getElementsByTagName('a')[0].click();
+
+      setTimeout(() => {
+        iframe.remove();
+        URL.revokeObjectURL(blobUrl);
+      }, 2000);
+    } catch (e) {
+      console.error('Download failed:', e);
+    }
   },
 };
 
